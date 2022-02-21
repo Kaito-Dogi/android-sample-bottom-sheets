8a2d089fde19caea65098f0388851a062f999b97
Kaito-Dogi <kendobu0405@gmail.com>
2022-02-22 00:10:19 +0900

    add: コールバック関数を定義

--- app/src/main/java/app/myoji/nickname/bottomsheetsample/MainActivity.kt
+++ app/src/main/java/app/myoji/nickname/bottomsheetsample/MainActivity.kt
@@ -2,7 +2,8 @@ package app.myoji.nickname.bottomsheetsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.view.View
 import app.myoji.nickname.bottomsheetsample.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
@@ -15,12 +16,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Use this to programmatically apply behavior attributes
         val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
 
-        binding.button.setOnClickListener {
+        // add the callback
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                val logMessage: String = when (newState) {
+                    BottomSheetBehavior.STATE_DRAGGING -> "DRAGGING"
+                    BottomSheetBehavior.STATE_SETTLING -> "SETTING"
+                    BottomSheetBehavior.STATE_EXPANDED -> "EXPANDED"
+                    BottomSheetBehavior.STATE_COLLAPSED -> "COLLAPSED"
+                    BottomSheetBehavior.STATE_HIDDEN -> "HIDDEN"
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> "HALF_EXPANDED"
+                    else -> ""
+                }
+                Log.d(BOTTOM_SHEET_STATE, logMessage)
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                when {
+                    slideOffset >= 0 -> binding.fab.setImageResource(R.drawable.ic_expand_more)
+                    slideOffset < 0 -> binding.fab.setImageResource(R.drawable.ic_expand_less)
+                }
+            }
+        })
+
+        binding.fab.setOnClickListener {
             when (bottomSheetBehavior.state) {
-                BottomSheetBehavior.STATE_HIDDEN -> bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                BottomSheetBehavior.STATE_EXPANDED -> bottomSheetBehavior.state =
+                    BottomSheetBehavior.STATE_COLLAPSED
+                BottomSheetBehavior.STATE_HIDDEN -> bottomSheetBehavior.state =
+                    BottomSheetBehavior.STATE_COLLAPSED
+                BottomSheetBehavior.STATE_COLLAPSED -> bottomSheetBehavior.state =
+                    BottomSheetBehavior.STATE_HIDDEN
             }
         }
+    }
+
+    companion object {
+        private const val BOTTOM_SHEET_STATE = "bottom_sheet_state"
     }
 }
